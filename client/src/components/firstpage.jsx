6bf35528968f29a69279b094b19c45149031dfe0
samih ringuet <samrin2@proton.me>
2024-07-01 15:42:48 +0200

    first Page finished

---
+++ client/src/components/firstpage.jsx
@@ -0,0 +1,14 @@
+
+function FirstPage  ()  {
+  return (
+    <div className="first-page">
+      <img src="src\assets\images\Group_57.png" alt="logo principal du site web représentant une fusion des differentes meteo" className="logoprincipal" />
+      <p className="headline"><strong>Your weather,</strong></p>
+      <p className="subheadline"><strong>always at hand!</strong></p>
+      <p className="textfirstpage">Don't let the weather<br></br> surprise you.</p>
+      <button type="button" className="lets-go-btn"><strong>Let's go!</strong></button>
+    </div>
+  );
+}
+
+export default FirstPage;
