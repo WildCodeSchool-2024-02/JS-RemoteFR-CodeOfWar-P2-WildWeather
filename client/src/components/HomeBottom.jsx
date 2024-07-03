3beedc5cf2ca30e4555d6a9009e78788ff7925aa
Monica Pardo <moonyruna3@gmail.com>
2024-07-03 15:29:15 +0200

    resolve api call and part of props

--- client/src/components/HomeBottom.jsx
+++ client/src/components/HomeBottom.jsx
@@ -10,25 +10,23 @@ function HomeBottom({ weather, userWeather }) {
         <div className="weather-data-col">
           <p>Humidite: </p>
           <h4>
+            {/* {userWeather.main && userWeather.main.humidity */}
             {userWeather.length !== 0
               ? userWeather.main.humidity
               : weather.main.humidity}
             %
           </h4>
-          {/* <h4>{weather.main && weather.main.humidity}%</h4> */}
         </div>
 
         <div className="weather-data-col">
           <p>Vent:</p>
           <h4>
-            {userWeather.length !== 0 &&
-            userWeather.wind &&
-            userWeather.wind.speed
+            {/* {userWeather.wind && userWeather.wind.speed */}
+            {userWeather.length !== 0
               ? userWeather.wind.speed
               : weather.wind.speed}
             m/s
           </h4>
-          {/* <h4>{weather.wind && weather.wind.speed} m/s</h4> */}
         </div>
       </div>
 
@@ -36,23 +34,22 @@ function HomeBottom({ weather, userWeather }) {
         <div className="weather-data-col">
           <p>T.max:</p>
           <h4>
-            {userWeather.length !== 0 && userWeather.main.temp_max
+            {/* {userWeather.main && userWeather.main.temp_max */}
+            {userWeather.length !== 0
               ? userWeather.main.temp_max
               : weather.main.temp_max}
             ºC
           </h4>
-
-          {/* <h4>{weather.main && Math.floor(weather.main.temp_max)}º C</h4> */}
         </div>
 
         <div className="weather-data-col">
           <p>T.min: </p>
           <h4>
+            {/* {userWeather.main && userWeather.main.temp_min */}
             {userWeather.length !== 0
               ? userWeather.main.temp_min
               : weather.main.temp_min}
           </h4>
-          {/* <h4>{weather.main && Math.floor(weather.main.temp_min)}º C</h4> */}
         </div>
       </div>
     </div>
@@ -70,18 +67,19 @@ HomeBottom.propTypes = {
       speed: PropTypes.number,
     }).isRequired,
   }).isRequired,
-};
-HomeBottom.propTypes = {
-  userWeather: PropTypes.shape({
-    main: PropTypes.shape({
-      humidity: PropTypes.number,
-      temp_max: PropTypes.number,
-      temp_min: PropTypes.number,
-    }).isRequired,
-    wind: PropTypes.shape({
-      speed: PropTypes.number,
-    }).isRequired,
-  }).isRequired,
+
+  userWeather: PropTypes.arrayOf(
+    PropTypes.shape({
+      main: PropTypes.shape({
+        humidity: PropTypes.number,
+        temp_max: PropTypes.number,
+        temp_min: PropTypes.number,
+      }).isRequired,
+      wind: PropTypes.shape({
+        speed: PropTypes.number,
+      }).isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export default HomeBottom;
