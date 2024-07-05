9b046cde310f2d3367f71c8a2e6d1b4c3a1c063d
Monica Pardo <moonyruna3@gmail.com>
2024-07-05 11:00:05 +0200

    finish linechart

--- client/src/components/LineCharts.jsx
+++ client/src/components/LineCharts.jsx
@@ -1,4 +1,5 @@
-import { LineChart, Line, Tooltip, XAxis, YAxis } from "recharts";
+import { LineChart, Line, Tooltip, XAxis, Area, AreaChart } from "recharts";
+import PropTypes from "prop-types";
 import "../style/linechart.css";
 
 export default function LineCharts({ forecast }) {
@@ -10,7 +11,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[0].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[0].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[0].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
     {
@@ -18,7 +18,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[1].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[1].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[1].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
     {
@@ -26,7 +25,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[2].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[2].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[2].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
     {
@@ -34,7 +32,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[3].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[3].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[3].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
     {
@@ -42,7 +39,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[4].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[4].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[4].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
     {
@@ -50,7 +46,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[5].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[5].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[5].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
     {
@@ -58,7 +53,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[6].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[6].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[6].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
     {
@@ -66,7 +60,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[7].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[7].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[7].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
     {
@@ -74,7 +67,6 @@ export default function LineCharts({ forecast }) {
         forecast.length !== 0 && `${forecast.list[8].dt_txt.substring(11, 16)}`,
       speed:
         forecast.length !== 0 && `${forecast.list[8].wind.speed.toFixed(1)}`,
-      gust: forecast.length !== 0 && `${forecast.list[8].wind.gust.toFixed(1)}`,
       amt: 2400,
     },
   ];
@@ -82,20 +74,33 @@ export default function LineCharts({ forecast }) {
     <>
       <p className="speed-title">Wind Speed: mph</p>
       <div className="linechart-container">
-        <LineChart
+        <AreaChart
           width={600}
           height={300}
           data={data}
-          margin={{ top: 0, right: 10, bottom: 5, left: 0 }}
+          margin={{ top: 0, right: 10, bottom: 5, left: 10 }}
         >
-          <Line type="monotone" dataKey="gust" stroke="red" />
-          <Line type="monotone" dataKey="speed" stroke="#0e0c5e" />
+          <XAxis dataKey="name" stroke="#0e0c5e" fontWeight={600} />
 
-          <XAxis dataKey="name" />
-          <YAxis />
           <Tooltip />
-        </LineChart>
+          <Area
+            type="monotone"
+            dataKey="speed"
+            stroke="#0e0c5e"
+            fill="#d5e8ff"
+          />
+        </AreaChart>
       </div>
     </>
   );
 }
+LineCharts.propTypes = {
+  forecast: PropTypes.shape({
+    length: PropTypes.number.isRequired,
+    list: PropTypes.shape({
+      wind: PropTypes.shape({
+        speed: PropTypes.number.isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
