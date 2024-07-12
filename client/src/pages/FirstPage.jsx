565ba73b6fadf2a0695b3effc205693228c6ac72
Damien <goguet.damien@hotmail.fr>
2024-07-12 11:03:53 +0200

    fix: Searchbar-TagYourCity and rename css files

--- client/src/pages/FirstPage.jsx
+++ client/src/pages/FirstPage.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "../style/firstpage.css";
+import { BackgroundProvider } from "../context/BackgroundContext";
+import "../style/backgroundcloud.css";
 
 function FirstPage() {
   const navigate = useNavigate();
@@ -8,20 +10,22 @@ function FirstPage() {
     navigate("/NameInput");
   };
   return (
-    <div className="first-page">
-      <img
-        src="src\assets\images\Group_57.png"
-        alt="logo princiapl"
-        className="logoprincipal"
-      />
-      <p className="headline">Your weather,</p>
-      <p className="subheadline">always at hand!</p>
-      <p className="textfirstpage1">Don't let the weather</p>
-      <p className="textfirstpage2">surprise you.</p>
-      <button type="button" className="lets-go-btn" onClick={goToNameInput}>
-        Let's go!
-      </button>
-    </div>
+    <BackgroundProvider>
+      <div className="first-page">
+        <img
+          src="src\assets\images\Group_57.png"
+          alt="logo princiapl"
+          className="logoprincipal"
+        />
+        <p className="headline">Your weather,</p>
+        <p className="subheadline">always at hand!</p>
+        <p className="textfirstpage1">Don't let the weather</p>
+        <p className="textfirstpage2">surprise you.</p>
+        <button type="button" className="lets-go-btn" onClick={goToNameInput}>
+          Let's go!
+        </button>
+      </div>
+    </BackgroundProvider>
   );
 }
 
